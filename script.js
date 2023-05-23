0acd2a5229b9b4bec12f62a182a0918a8acd6ec0
DiMNDev <Dimndev@gmail.com>
2023-05-22 23:01:59 -0600

    my clock

--- script.js
+++ script.js
@@ -1,22 +1,29 @@
 const hour = document.querySelector(".hour");
 const minute = document.querySelector(".minute");
 const second = document.querySelector(".second");
+const stringDate = document.querySelector(".string-date");
 
 function setDate() {
   const now = new Date();
-  console.log(now);
-  const getSecond = now.getSeconds();
+  const getSeconds = now.getSeconds();
   const getMinutes = now.getMinutes();
   const getHours = now.getHours();
 
-  const secondDegree = (getSecond / 60) * 360;
+  const secondDegree = (getSeconds / 60) * 360;
   const minuteDegree = (getMinutes / 60) * 360;
   const hourDegree = (getHours / 12) * 360;
-  console.log(secondDegree);
+
   second.style.transform = `rotate(${secondDegree}deg)`;
   second.style.transition = "all .5s";
   minute.style.transform = `rotate(${minuteDegree}deg)`;
   hour.style.transform = `rotate(${hourDegree}deg)`;
+  stringDate.innerHTML = now.toDateString();
+  const hourChar = document.querySelector(".time-counter.hour");
+  const minuteChar = document.querySelector(".time-counter.minute");
+  const secondChar = document.querySelector(".time-counter.second");
+  hourChar.innerHTML = getHours + " :";
+  minuteChar.innerHTML = getMinutes + " :";
+  secondChar.innerHTML = getSeconds + "";
 }
 
 setInterval(setDate, 1000);
